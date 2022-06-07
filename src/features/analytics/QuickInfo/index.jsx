546930ea8a94de9de161b3ab096ad03dbb4890b3
Shou Zhang <shoud.zhang@gmail.com>
2022-06-06 18:09:28 -0700

    fixed error in seed

--- src/features/analytics/QuickInfo/index.jsx
+++ src/features/analytics/QuickInfo/index.jsx
@@ -1,21 +1,32 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import colors from '../../../constants/colors';
 import moment from 'moment';
 import Item from './styled';
 import { useSelector } from 'react-redux';
-import { Chart as ChartJS, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
 import { MenuItem, FormControl, Select, Box, Grid } from '@mui/material';
-
-ChartJS.register(Title, BarElement, Tooltip, Legend);
+import StackedBarGraph from './StackedBarGraph';
 
 function QuickInfo({ selectedYear, setSelectedYear, records }) {
   const currentTime = moment().format('h:mm a');
+  const [w, setW] = useState(window.innerWidth);
   const [clockTime, setClockTime] = useState(currentTime);
 
   setInterval(() => {
     setClockTime(moment().format('h:mm a'));
   }, 1000);
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log(window.innerWidth);
+      setW(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const entries = useSelector(({ journal }) => journal.entries);
 
@@ -51,7 +62,7 @@ function QuickInfo({ selectedYear, setSelectedYear, records }) {
     return returnedMonthObj;
   }, [entries]);
 
-  const monthDifferenceDisplay = () => {
+  const monthDifferenceMessage = () => {
     if (monthObj[selectedYear][lastMonth] !== (undefined || 0)) {
       const difference = Math.round(
         (monthObj[selectedYear][currentMonth] /
@@ -86,7 +97,7 @@ function QuickInfo({ selectedYear, setSelectedYear, records }) {
     );
   };
 
-  const handleChange = (e) => {
+  const handleYearChange = (e) => {
     const indexNum = e.target.value / 10;
     setSelectedYear(Object.keys(records)[indexNum]);
   };
@@ -96,26 +107,64 @@ function QuickInfo({ selectedYear, setSelectedYear, records }) {
       return years === selectedYear;
     }) * 10;
 
+  if (w > 1100) {
+    return (
+      <>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={1}>
+            <Grid item xs={8}>
+              <Item>{currentDay}</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>{clockTime}</Item>
+            </Grid>
+            <Grid item xs={8}>
+              <Item>{monthDifferenceMessage()}</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>
+                <FormControl sx={{ autoWidth: true, fontSize: 1 }} size="small">
+                  <Select
+                    value={valueOfSelectedYear}
+                    sx={{ fontSize: 14, color: colors.blue1 }}
+                    onChange={handleYearChange}
+                  >
+                    {Object.keys(records)
+                      .sort()
+                      .map((year, i) => (
+                        <MenuItem key={i} value={i * 10}>
+                          {year}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Item sx={{ height: 150 }}>
+                <StackedBarGraph
+                  selectedYear={selectedYear}
+                  monthObj={monthObj}
+                />
+              </Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </>
+    );
+  }
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={1}>
-          <Grid item xs={8}>
-            <Item>{currentDay}</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>{clockTime}</Item>
-          </Grid>
-          <Grid item xs={8}>
-            <Item>{monthDifferenceDisplay()}</Item>
-          </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={12}>
             <Item>
               <FormControl sx={{ autoWidth: true, fontSize: 1 }} size="small">
                 <Select
                   value={valueOfSelectedYear}
-                  sx={{ fontSize: 17, color: colors.blue1 }}
-                  onChange={handleChange}
+                  sx={{ fontSize: 20, color: colors.blue1 }}
+                  onChange={handleYearChange}
                 >
                   {Object.keys(records)
                     .sort()
@@ -129,111 +178,10 @@ function QuickInfo({ selectedYear, setSelectedYear, records }) {
             </Item>
           </Grid>
           <Grid item xs={12}>
-            <Item sx={{ height: 150 }}>
-              <Bar
-                options={{
-                  indexAxis: 'y',
-                  scales: {
-                    xAxes: {
-                      stacked: true,
-                      ticks: {
-                        stepSize: 20,
-                      },
-                    },
-                    yAxes: {
-                      stacked: true,
-                    },
-                  },
-                  plugins: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    title: {
-                      display: true,
-                      text: 'Entry Count by Month',
-                    },
-                    legend: {
-                      display: false,
-                    },
-                  },
-                  maintainAspectRatio: false,
-                }}
-                data={{
-                  labels: [['Total', 'Entry', 'Count']],
-                  datasets: [
-                    {
-                      label: 'January',
-                      data: [monthObj[selectedYear][1]],
-                      backgroundColor: '#D9ED92',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'Febuary',
-                      data: [monthObj[selectedYear][2]],
-                      backgroundColor: '#B5E48C',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'March',
-                      data: [monthObj[selectedYear][3]],
-                      backgroundColor: '#99D98C',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'April',
-                      data: [monthObj[selectedYear][4]],
-                      backgroundColor: '#76C893',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'May',
-                      data: [monthObj[selectedYear][5]],
-                      backgroundColor: '#52B69A',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'June',
-                      data: [monthObj[selectedYear][6]],
-                      backgroundColor: '#34a493',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'July',
-                      data: [monthObj[selectedYear][7]],
-                      backgroundColor: '#34A0A4',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'August',
-                      data: [monthObj[selectedYear][8]],
-                      backgroundColor: '#348fa4',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'September',
-                      data: [monthObj[selectedYear][9]],
-                      backgroundColor: '#168AAD',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'October',
-                      data: [monthObj[selectedYear][10]],
-                      backgroundColor: '#1A759F',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'November',
-                      data: [monthObj[selectedYear][11]],
-                      backgroundColor: '#1E6091',
-                      borderWidth: 0,
-                    },
-                    {
-                      label: 'December',
-                      data: [monthObj[selectedYear][12]],
-                      backgroundColor: '#184E77',
-                      borderWidth: 0,
-                    },
-                  ],
-                }}
+            <Item sx={{ height: 'auto', mt: 2 }}>
+              <StackedBarGraph
+                selectedYear={selectedYear}
+                monthObj={monthObj}
               />
             </Item>
           </Grid>
