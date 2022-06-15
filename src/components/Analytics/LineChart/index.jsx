08ba2a7ba0d4e8a2b661f615463fde8f2885f28c
Shou Zhang <shoud.zhang@gmail.com>
2022-06-15 14:14:03 -0700

    improved modularity of line graph component

--- src/components/Analytics/LineChart/index.jsx
+++ src/components/Analytics/LineChart/index.jsx
@@ -24,8 +24,13 @@ ChartJS.register(
   PointElement
 );
 
-export default function LineChart({ records, selectedYear, currentMonth }) {
-  if (Object.keys(records).length < 1) {
+export default function LineChart({
+  isRecordsEmpty,
+  selectedYear,
+  currentMonth,
+  lineGraphData,
+}) {
+  if (isRecordsEmpty) {
     return <CircularProgress></CircularProgress>;
   }
 
@@ -33,44 +38,7 @@ export default function LineChart({ records, selectedYear, currentMonth }) {
     <Line
       data={{
         labels: months.slice(0, currentMonth),
-        datasets: [
-          {
-            label: 'Loved',
-            data: Object.values(records[selectedYear].loved.wordCount),
-            backgroundColor: colors.variantMap.loved,
-            borderColor: `${colors.variantMap.loved}50`,
-          },
-          {
-            label: 'Happy',
-            data: Object.values(records[selectedYear].happy.wordCount),
-            backgroundColor: colors.variantMap.happy,
-            borderColor: `${colors.variantMap.happy}50`,
-          },
-          {
-            label: 'Calm',
-            data: Object.values(records[selectedYear].calm.wordCount),
-            backgroundColor: colors.variantMap.calm,
-            borderColor: `${colors.variantMap.calm}50`,
-          },
-          {
-            label: 'Sad',
-            data: Object.values(records[selectedYear].sad.wordCount),
-            backgroundColor: colors.variantMap.sad,
-            borderColor: `${colors.variantMap.sad}50`,
-          },
-          {
-            label: 'Anxious',
-            data: Object.values(records[selectedYear].anxious.wordCount),
-            backgroundColor: colors.variantMap.anxious,
-            borderColor: `${colors.variantMap.anxious}50`,
-          },
-          {
-            label: 'Angry',
-            data: Object.values(records[selectedYear].angry.wordCount),
-            backgroundColor: colors.variantMap.angry,
-            borderColor: `${colors.variantMap.angry}50`,
-          },
-        ],
+        datasets: lineGraphData,
       }}
       options={{
         plugins: {
